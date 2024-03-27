e0c11a53b422c32e7746d04aeffe87b94665ce4e
GGDiniz <gabrieldinizb35@gmail.com>
2024-03-27 13:56:11 -0300

    Alterações dos textos

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // animacao navbar
 const nav = document.querySelector('.topnav')
 window.addEventListener('scroll', fixNav)
@@ -41,51 +40,3 @@ function ham() {
     x.style.fontSize = "12px";
   }
 }
-
-
-=======
-// animacao navbar
-const nav = document.querySelector('.topnav')
-window.addEventListener('scroll', fixNav)
-
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
-}
-//slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].style.justify_content = "center";
-}
-function ham() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-    document.querySelector('.topnav').style.height = "80px";
-  } else {
-    x.style.display = "flex";
-    document.querySelector('.topnav').style.height = "150px";
-    x.style.fontSize = "12px";
-  }
-}
-
-
->>>>>>> fff8d478b6c2d06cbbabb59cce527cce52584ad1
